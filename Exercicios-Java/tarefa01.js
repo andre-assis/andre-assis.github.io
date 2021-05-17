25d869f5da5f97af937d7c86b45230d6b4330de3
Andre Pinto <andrepinto.cg@gmail.com>
2021-05-17 09:35:37 -0300

    att java exercicio

--- Exercicios-Java/tarefa01.js
+++ Exercicios-Java/tarefa01.js
@@ -1,4 +1,19 @@
-var numero1 = 3;
-var numero2 = 4;
+(function(win,doc){
+    'use strict';
 
-console.log (numero1 + numero2);
+    doc.querySelector('#submit').addEventListener('click', function(event){
+        event.preventDefault();
+        let allInputs = doc.querySelectorAll('input[type=text]');
+        let sum = 0;
+
+        for(let i=0; i < allInputs.length; i++){
+            let value=allInputs[i].value;
+            sum += parseInt(value);
+        }
+
+        doc.querySelector('.result').classList.remove('d-none');
+        doc.querySelector('.sum').innerHTML='Soma : ${sum}';
+
+}, false);
+
+})(window,document)
